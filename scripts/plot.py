f83b4b7cd1b8008d6de914e3fa180ce3941473a2
zoj <44142765+zoj613@users.noreply.github.com>
2021-01-24 23:41:06 +0200

    DOC: Update density plot in readme

--- scripts/plot.py
+++ scripts/plot.py
@@ -5,15 +5,29 @@ import numpy as np
 import seaborn as sns
 
 from polyagamma import polyagamma
+from pypolyagamma import PyPolyaGamma
 
 sns.set_style("darkgrid")
-rng = np.random.default_rng(1)
 
-data = {"devroye": None, "alternate": None, "gamma": None, "saddle": None}
+rng = np.random.default_rng(0)
+pg = PyPolyaGamma(0)
+
+data = {
+    "devroye": None,
+    "alternate": None,
+    "gamma": None,
+    "saddle": None,
+    "$pypolyagamma$": None
+}
 
 def plot_densities(h=1, z=0, size=1000):
     for method in data:
-        data[method] = polyagamma(h=h, z=z, method=method, size=size, random_state=rng)
+        if method == "$pypolyagamma$":
+            data[method] = [pg.pgdraw(h, z) for _ in range(size)]
+        else:
+            data[method] = polyagamma(
+                h=h, z=z, method=method, size=size, random_state=rng
+            )
     sns.kdeplot(data=data)
 
 if __name__ == "__main__":
@@ -26,6 +40,7 @@ if __name__ == "__main__":
 
     plt.title(
         f'Density plots of PG(h, {args.z})' +
-        ' using each method for h $\in$ {1,4,7,10,15}.'
+        ' using each method for h $\in$ {1,4,7,10,15}. \nThe package ' +
+        '$pypolyagamma$ is used as a reference.'
     )
     plt.savefig("./densities.svg")
