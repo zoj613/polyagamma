FIX: Fix up some numerical precision issues with alternate method and generate better truncation points

scripts/generate_alternate_truncation_pts.py
@@ -5,31 +5,23 @@ src/pgm_alternate_trunc_points.h. These are intended to be used when sampling
 from a Polya-gamma distribution using the Alternate method. Values of h in this
 interval not present in the table are calculated using a linear interpolation.
 
-We find the roots the function f(x) =  l(x|h) - r(x|h) for a given value of h.
+We minimize the function f(x) =  p(x|h) + q(x|h) for a given value of h.
 """
+import argparse
 from datetime import datetime
 from math import log, pi, exp
 import numpy as np
 
-from scipy.optimize import root_scalar
-from scipy.special import loggamma
+from scipy.optimize import minimize_scalar
+from scipy.special import gammaincc
 
 log2 = log(2)
-twopi = 2 * pi
-logpi_2 = log(0.5 * pi)
-pi2_8 = 0.125 * (pi ** 2)
-
-
-def l(x, h):
-    return exp(h * log2 + log(h) - 0.5 * log(twopi * x * x * x) - 0.5 * (h ** 2) / x)
-
-
-def r(x, h):
-    return exp(0.5 * h * logpi_2 + (h - 1) * log(x) - pi2_8 * x - loggamma(h))
+log4_pi = log(4 / pi)
 
 
 def f(x, h=1):
-    return l(x, h) - r(x, h)
+    return (exp(h * log2) * gammaincc(0.5, 0.5 * h * h / x) +
+            exp(h * log4_pi) * gammaincc(h, x * pi * pi / 8))
 
 
 def chunk(arr, size=3):
@@ -59,18 +51,23 @@ def formatted(arr, name):
 
 
 if __name__ == "__main__":
-    n = 50
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--verbose', action='store_true', default=False)
+    parser.add_argument('--size', type=int, default=25)
+    parser.add_argument('--maxh', type=int, default=4)
+    parser.add_argument('--scale', type=float, default=2)
+    args = parser.parse_args()
+    n = args.size
     h_val = np.zeros(n)
     f_val = np.zeros(n)
     count = 0
-    # find roots of f given each value of h in [1, 4]
-    for i in np.linspace(1, 4, n):
-        sol = root_scalar(f, args=(i,), bracket=[0.001, 20]).root
+    for i in np.linspace(1, args.maxh, n):
+        sol = minimize_scalar(f, method='bounded', bounds=[1e-200, 10000], args=(i,)).x
+        if args.verbose:
+            print(f"h={i} | root={sol} | f(root)={f(sol)} | f(scaled_root)={f(args.scale * sol)}")
         h_val[count] = i
-        f_val[count] = sol
+        f_val[count] = args.scale * sol
         count += 1
-
-    h_range = h_val.max() - h_val.min()
 
     np.set_printoptions(formatter={'float': lambda x: "{0:0.9f}".format(x)})
     with open('./src/pgm_alternate_trunc_points.h', 'w') as f:
@@ -78,7 +75,7 @@ if __name__ == "__main__":
         f.write(f"Last generated: {datetime.now()} */\n")
         f.write("\n")
         f.write(f"static const size_t pgm_table_size = {n};\n")
-        f.write(f"static const double pgm_h_range = {h_range};")
+        f.write(f"static const size_t pgm_maxh = {args.maxh};")
         f.write("\n\n")
         f.write(formatted(h_val, 'pgm_h'))
         f.write('\n\n')