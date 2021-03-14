73d43aa7cc0f90baed83eb85c3fa80b3f66faa97
zoj <44142765+zoj613@users.noreply.github.com>
2021-03-14 18:52:51 +0200

    MAINT: replace extern with numpy's DECLDIR

--- src/pgm_common.c
+++ src/pgm_common.c
@@ -4,11 +4,11 @@
 #include "pgm_common.h"
 
 
-extern NPY_INLINE double
+DECLDIR NPY_INLINE double
 pgm_erfc(double x);
 
-extern NPY_INLINE double
+DECLDIR NPY_INLINE double
 pgm_lgamma(double z);
 
-extern NPY_INLINE double
+DECLDIR NPY_INLINE double
 random_left_bounded_gamma(bitgen_t* bitgen_state, double a, double b, double t);
