b2f4811f1d47ebf2e0b00769b2ddd3c4436aaede
zoj <44142765+zoj613@users.noreply.github.com>
2021-03-28 12:05:13 +0200

    ENH: add type annotation stubs

---
+++ polyagamma/__init__.pyi
@@ -0,0 +1,9 @@
+from ._polyagamma import (
+    polyagamma_logpdf as polyagamma_logpdf,
+    polyagamma_logcdf as polyagamma_logcdf,
+    polyagamma_pdf as polyagamma_pdf,
+    polyagamma_cdf as polyagamma_cdf,
+    random_polyagamma as random_polyagamma,
+)
+
+__version__ : str
