39a2e98743281e8cd3a9641bed72cc0df84d94b3
zoj <44142765+zoj613@users.noreply.github.com>
2021-04-11 21:32:58 +0200

    MAINT: Cleanup devroye module and use single precision float where possible

--- src/pgm_devroye.c
+++ src/pgm_devroye.c
@@ -10,28 +10,29 @@
 /* a struct to store frequently used values. This avoids unnecessary
  * recalculation of these values during a single call to the sampler.
  */
-struct config {
-    double z;
-    double z2;
-    double k;
-    double ratio;
-    double x;
+typedef struct {
+    double proposal_probability;
     double logx;
-};
+    double z2;
+    double z;
+    double k;
+    double x;
+} parameter_t;
 
 /* 
  * Compute a_n(x|t), the nth term of the alternating sum S_n(x|t)
  */
-static NPY_INLINE double
-piecewise_coef(size_t n, struct config* cfg)
+static NPY_INLINE float
+piecewise_coef(int n, parameter_t const* pr)
 {
-    if (cfg->x > T) {
+    if (pr->x > T) {
         double b = NPY_PI * (n + 0.5);
-        return b * exp(-0.5 * cfg->x * b * b);
+        return (float)b * expf(-0.5 * pr->x * b * b);
     }
-    else if (cfg->x > 0) {
+    else if (pr->x > 0) {
         double a = n + 0.5;
-        return NPY_PI * a * exp(-1.5 * (PGM_LOGPI_2 + cfg->logx) - 2 * a * a / cfg->x);
+        return expf(-1.5 * (PGM_LOGPI_2 + pr->logx) - 2. * a * a / pr->x) *
+               (float)(NPY_PI * a);
     }
     return 0;
 }
@@ -43,36 +44,38 @@ piecewise_coef(size_t n, struct config* cfg)
  * Setting mu=Inf if z=0 ensures that sampling from a truncated inverse-gaussian
  * uses most efficient sampling algorithm in `random_right_bounded_inverse_gaussian`.
  *
- * Note that we do not need to calculate the cdf of the inverse gaussian in
- * order to get the value of `p`. Since the value of the cdf is always
- * evaluated at the trancation point T, its value is only dependent on `z`.
- * Thus we simplify the expression for p and arrive at:
+ * NOTE
+ * ----
+ * We do not need to calculate the cdf of the inverse gaussian in order to get
+ * the value of `p`. Since the value of the cdf is always evaluated at the
+ * trancation point T, its value is only dependent on `z`. Thus we simplify the
+ * expression for p and arrive at:
  *      p = erfc(a - b) / exp(z) + exp(z) * erfc(a + b),
  * where a = 1 / sqrt(2 * T) and b = z * sqrt(T/2).
  */
 static NPY_INLINE void
-initialize_config(struct config* cfg, double z)
+set_sampling_parameters(parameter_t* const pr, double z)
 {
     if (z > 0) {
-        static const double a = 0.8838834764831844;  // 1 / sqrt(2 * T)
-        static const double t_two = 0.565685424949238;  // sqrt(T/2)
+        static const float a = 0.8838834764831844f;  // 1 / sqrt(2 * T)
+        static const float t_two = 0.565685424949238f;  // sqrt(T/2)
         double p, q;
-        double b = z * t_two;
-        double ez = exp(z);
+        float b = z * t_two;
+        float ez = expf(z);
 
         p = pgm_erfc(a - b) / ez + pgm_erfc(a + b) * ez;
-        cfg->z2 = z * z;
-        cfg->k = PGM_PI2_8 + 0.5 * cfg->z2;
-        q = NPY_PI_2 * exp(-cfg->k * T) / cfg->k;
-        cfg->ratio = p / (p + q);
+        pr->z2 = z * z;
+        pr->k = PGM_PI2_8 + 0.5 * pr->z2;
+        q = NPY_PI_2 * expf(-pr->k * T) / pr->k;
+        pr->proposal_probability = p / (p + q);
     }
     else {
-        cfg->k = PGM_PI2_8;
-        cfg->ratio = 0.4223027567786595;
-        cfg->z2 = 0;
+        pr->proposal_probability = 0.4223027567786595;
+        pr->k = PGM_PI2_8;
+        pr->z2 = 0.;
     }
-    cfg->z = z;
-    cfg->logx = 0;
+    pr->logx = 0.;
+    pr->z = z;
 }
 
 /*
@@ -104,30 +107,28 @@ initialize_config(struct config* cfg, double z)
  *      http://hdl.handle.net/2152/21842
  */
 static NPY_INLINE double
-random_right_bounded_invgauss(bitgen_t* bitgen_state, struct config* cfg)
+random_right_bounded_invgauss(bitgen_t* bitgen_state, parameter_t* const pr)
 {
-    static const double t_inv = 1.5625;  // 1 / T
-    static const double two_t = 3.125;  // 2 / T
     double x;
-
-    if (cfg->z < t_inv) {
+    // 1 / T = 1.5625
+    if (pr->z < 1.5625) {
         do {
             double e1, e2;
             do {
                 e1 = random_standard_exponential(bitgen_state);
                 e2 = random_standard_exponential(bitgen_state);
-            } while ((e1 * e1) > (two_t * e2));
+            } while (e1 * e1 > 3.125 * e2);  // 2 / T = 3.125
             x = (1 + T * e1);
             x = T / (x * x);
-        } while (cfg->z > 0 && log1p(-next_double(bitgen_state)) >= -0.5 * cfg->z2 * x);
+        } while (pr->z > 0 && log1pf(-next_float(bitgen_state)) >= -0.5 * pr->z2 * x);
         return x;
     }
     do {
         double y = random_standard_normal(bitgen_state);
-        double w = (cfg->z + 0.5 * y * y) / cfg->z2;
-        x = w - sqrt(w * w - 1 / cfg->z2);
-        if (next_double(bitgen_state) * (1 + x * cfg->z) > 1) {
-            x = 1 / (x * cfg->z2);
+        double w = (pr->z + 0.5 * y * y) / pr->z2;
+        x = w - sqrt(w * w - 1 / pr->z2);
+        if (next_double(bitgen_state) * (1. + x * pr->z) > 1.) {
+            x = 1. / (x * pr->z2);
         }
     } while (x >= T);
     return x;
@@ -140,26 +141,26 @@ random_right_bounded_invgauss(bitgen_t* bitgen_state, struct config* cfg)
  * Polson et al.(2013) and page 14 of Windle et al. (2014).
  */
 static NPY_INLINE double
-random_jacobi(bitgen_t* bitgen_state, struct config* cfg)
+random_jacobi_star(bitgen_t* bitgen_state, parameter_t* const pr)
 {
     for (;;) {
-        if (next_double(bitgen_state) < cfg->ratio) {
-            cfg->x = random_right_bounded_invgauss(bitgen_state, cfg);
-            cfg->logx = log(cfg->x);
+        if (next_double(bitgen_state) < pr->proposal_probability) {
+            pr->x = random_right_bounded_invgauss(bitgen_state, pr);
+            pr->logx = logf(pr->x);
         }
         else {
-            cfg->x = T + random_standard_exponential(bitgen_state) / cfg->k;
+            pr->x = T + random_standard_exponential(bitgen_state) / pr->k;
         }
-        double s = piecewise_coef(0, cfg);
-        double u = next_double(bitgen_state) * s;
-        for (size_t i = 1;; ++i) {
+        float s = piecewise_coef(0, pr);
+        float u = next_float(bitgen_state) * s;
+        for (int i = 1;; ++i) {
             if (i & 1) {
-                s -= piecewise_coef(i, cfg);
+                s -= piecewise_coef(i, pr);
                 if (u <= s)
-                    return cfg->x;
+                    return pr->x;
             }
             else {
-                s += piecewise_coef(i, cfg);
+                s += piecewise_coef(i, pr);
                 if (u > s)
                     break;
             }
@@ -174,13 +175,13 @@ random_jacobi(bitgen_t* bitgen_state, struct config* cfg)
 double
 random_polyagamma_devroye(bitgen_t* bitgen_state, double h, double z)
 {
-    struct config cfg;
+    parameter_t pr;
+    int n = h;
     double out = 0;
-    size_t n = h;
 
-    initialize_config(&cfg, z);
+    set_sampling_parameters(&pr, z);
     while (n--) {
-        out += random_jacobi(bitgen_state, &cfg);
+        out += random_jacobi_star(bitgen_state, &pr);
     }
     return 0.25 * out; 
 }
