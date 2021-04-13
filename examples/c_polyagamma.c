bae094c1794da04a05df22b32d7ec8d89a276385
zoj <44142765+zoj613@users.noreply.github.com>
2021-04-13 17:57:59 +0200

    DOC: update example C program

--- examples/c_polyagamma.c
+++ examples/c_polyagamma.c
@@ -1,15 +1,22 @@
 /*
+ * Copyright (c) 2020-2021, Zolisa Bleki
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * This module shows an examples of how to use polyagamma in a C program.
  * Here we use a custom bitgenerator called Xoroshiro128plus. Numpy's
  * bitgenerator struct requires defining function pointers for generating
  * integers and standard uniform numbers. We define these functions alongside the
  * bitgenerator.
  *
+ * The Xoroshiro128p code is derived from the bitgenerator author's original
+ * code found at: https://xorshift.di.unimi.it/xoroshiro128plus.c
+ *
  * This example can be compiled with:
  *
  * gcc examples/c_polyagamma.c src/*.c -I./include -I$(python -c "import numpy; print(numpy.get_include())") \
  *  -I/usr/include/python3.9 -L$(python -c "import numpy; print(numpy.get_include())")/../../random/lib \
- *  -lm -lnpyrandom -O3
+ *  -lm -lnpyrandom -O2 -march=native
  */
 #include "../include/pgm_random.h"
 
@@ -17,7 +24,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef struct {uint64_t s[2];} xrs128p_random_t;
+typedef struct {uint64_t s[2]; uint32_t s2[4];} xrs128p_random_t;
 
 
 static inline uint64_t
@@ -27,8 +34,15 @@ rotl(const uint64_t x, int k)
 }
 
 
+static inline uint32_t
+rotl32(const uint32_t x, int k)
+{
+	return (x << k) | (x >> (32 - k));
+}
+
+
 static inline uint64_t
-xrs128p_next_int(void* rng)
+xrs128p_next64(void* rng)
 {
     xrs128p_random_t* xrs = rng;
     const uint64_t s0 = xrs->s[0];
@@ -43,11 +57,31 @@ xrs128p_next_int(void* rng)
 }
 
 
+static inline uint32_t
+xrs128p_next32(void* rng)
+{
+    xrs128p_random_t* xrs = rng;
+	const uint32_t result = xrs->s2[0] + xrs->s2[3];
+	const uint32_t t = xrs->s2[1] << 9;
+
+	xrs->s2[2] ^= xrs->s2[0];
+	xrs->s2[3] ^= xrs->s2[1];
+	xrs->s2[1] ^= xrs->s2[2];
+	xrs->s2[0] ^= xrs->s2[3];
+
+	xrs->s2[2] ^= t;
+
+	xrs->s2[3] = rotl32(xrs->s2[3], 11);
+
+	return result;
+}
+
+
 static inline double
 xrs128p_next_double(void* rng)
 {
     xrs128p_random_t* xrs = rng;
-    return (xrs128p_next_int(xrs) >> 11) * (1.0 / 9007199254740992.0);
+    return (xrs128p_next64(xrs) >> 11) * (1.0 / 9007199254740992.0);
 }
 
 /*
@@ -59,14 +93,18 @@ main(void)
 {
     size_t n = 100;
     double* out = malloc(n * sizeof(*out));
-    // setup the bitgen
-    xrs128p_random_t xrs = {.s = {12132233, 1959324}};
+    // setup the bitgen with a specified seed
+    xrs128p_random_t xrs = {
+        .s = {12132233, 1959324}, .s2 = {25343, 98549, 78230, 91821}
+    };
     bitgen_t bitgen;
+
     bitgen.state = &xrs;
     bitgen.next_double = xrs128p_next_double;
-    bitgen.next_uint64 = xrs128p_next_int;
-
+    bitgen.next_uint64 = xrs128p_next64;
+    bitgen.next_uint32 = xrs128p_next32;
     pgm_random_polyagamma_fill(&bitgen, 1, 1.5, ALTERNATE, n, out);
+
     puts("Samples: [ ");
     for (size_t i = 0; i < n; i++)
        printf("%lf ", out[i]);
