fac595d84f9d6a894215d5ba2a3dd7f7ae5178bb
Zolisa Bleki <44142765+zoj613@users.noreply.github.com>
2022-06-23 11:42:37 +0200

    CI: Use `setuptools-scm` for automatic release version control.

--- polyagamma/__init__.py
+++ polyagamma/__init__.py
@@ -1,5 +1,4 @@
 from ._polyagamma import (
     polyagamma as random_polyagamma, polyagamma_pdf, polyagamma_cdf
 )
-
-__version__ = '1.3.3'
+from ._version import version as __version__, version_tuple as __version_info__
